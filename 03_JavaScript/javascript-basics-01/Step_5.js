d2a9ea2b889acb7582bf3a9835d5dcadabfac033
hasanelmestrah <hasanelmestrah@gmail.com>
2022-12-08 12:07:31 +0200

    step5

---
+++ 03_JavaScript/javascript-basics-01/Step_5.js
@@ -0,0 +1,11 @@
+function myfunc() {
+
+
+      var first_number = document.getElementById("first_number").value;
+      var second_number = document.getElementById("second_number").value;
+
+
+      alert("Multiply result:" + first_number * second_number);
+
+
+}
