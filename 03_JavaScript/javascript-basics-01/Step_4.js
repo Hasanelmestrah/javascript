b3b68efb95d873a9b633ed1633eda42a9a1697c7
hasanelmestrah <hasanelmestrah@gmail.com>
2022-12-08 11:54:58 +0200

    step4

---
+++ 03_JavaScript/javascript-basics-01/Step_4.js
@@ -0,0 +1,13 @@
+
+function myfunc() {
+
+
+var name=document.getElementById("name").value;
+var surname=document.getElementById("surname").value;
+var city=document.getElementById("city").value;
+
+
+alert(" Name:"+name+"\n surname:"+surname+"\n city:"+city );
+
+
+}
