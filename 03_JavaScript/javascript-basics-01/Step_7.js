eb628051bfada22c9c39437f232f40ec836abd91
hasanelmestrah <hasanelmestrah@gmail.com>
2022-12-08 13:21:06 +0200

    Step7

---
+++ 03_JavaScript/javascript-basics-01/Step_7.js
@@ -0,0 +1,11 @@
+function myfunc() {
+
+
+      var shoe_size = document.getElementById("shoe_size").value;
+      var year = document.getElementById("year").value;
+
+
+      alert(" shoe_size result:"+shoe_size*2+5*50+"\n year result:"+year/2+1766);
+
+
+}
