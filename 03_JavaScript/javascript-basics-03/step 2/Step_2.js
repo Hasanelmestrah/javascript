5e6cc3c787054c97160cc4c7cdfff4ebc507e4f2
hasanelmestrah <hasanelmestrah@gmail.com>
2022-12-09 15:35:40 +0200

    Step2

---
+++ 03_JavaScript/javascript-basics-03/step 2/Step_2.js
@@ -0,0 +1,10 @@
+
+
+function showhide(use) {
+      var texte = document.getElementById("texte");
+      if (use === "show") {
+            texte.style.display = "block";
+      } else if (use === "hide") {
+            texte.style.display = "none";
+      }
+}
